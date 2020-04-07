2288249a987587be7456b17621dde7aa0e9652ca
Francesco Giorgio <francesco@giorgio.dev>
2020-04-07 19:55:42 +0200

    refactor: Created components base structures
    Created components files and divided JSX structure in each component.
    No changes in contents are made.

---
+++ src/Categories.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import BooksList from "./BooksList"
+
+class Categories extends React.Component {
+    render() {
+        return (
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Currently Reading</h2>
+                            <div className="bookshelf-books">
+                                <BooksList />
+                            </div>
+                        </div>
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Want to Read</h2>
+                            <div className="bookshelf-books">
+                                <BooksList />
+                            </div>
+                        </div>
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Read</h2>
+                            <div className="bookshelf-books">
+                                <BooksList />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="open-search">
+                    <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Categories
